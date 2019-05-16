cc4cc60c90682fa7cc81a692dc887bd57a0550a9
Sven van der Meer <vdmeer.sven@mykolab.com>
2019-05-16 17:34:00 +0100

    writing postscript, adapting guide and examples and site

--- src/main/lcn-postscript/build.sh
+++ src/main/lcn-postscript/build.sh
@@ -1,6 +1,5 @@
-make clean
-lualatex lcn-postscript
-biber lcn-postscript
-makeindex lcn-postscript
-lualatex lcn-postscript
-lualatex lcn-postscript
+
+latexmk -gg -lualatex -bibtex 
+rm *.fls
+
+# -diagnostics
